7ba3550aaddb4b3042f3c592df1228595f5a2cd5
Brendan Abbott <brendan@bloodbone.ws>
2011-08-24 00:11:00 +1000

    Updates for the upcoming 2.3 release

--- extension.driver.php
+++ extension.driver.php
@@ -1,17 +1,18 @@
 <?php
-	
+
 	class Extension_ProfileDevKit extends Extension {
 	/*-------------------------------------------------------------------------
 		Definition:
 	-------------------------------------------------------------------------*/
-		
+
 		public static $active = false;
-		
+		public static $log = array();
+
 		public function about() {
 			return array(
 				'name'			=> 'Profile Devkit',
-				'version'		=> '1.0.4',
-				'release-date'	=> '2010-03-19',
+				'version'		=> '1.0.5pre',
+				'release-date'	=> 'unreleased',
 				'author'		=> array(
 					'name'			=> 'Rowan Lewis',
 					'website'		=> 'http://rowanlewis.com/',
@@ -19,7 +20,7 @@
 				)
 			);
 		}
-		
+
 		public function getSubscribedDelegates() {
 			return array(
 				array(
@@ -31,28 +32,41 @@
 					'page'		=> '/frontend/',
 					'delegate'	=> 'ManipulateDevKitNavigation',
 					'callback'	=> 'manipulateDevKitNavigation'
-				)
+				),
+				array(
+					'page' => '/frontend/',
+					'delegate' => 'LogQuery',
+					'callback' => 'logQuery'
+				),
 			);
 		}
-		
-		public function frontendDevKitResolve($context) {
+
+		public function frontendDevKitResolve(array $context) {
 			if (isset($_GET['profile'])) {
 				require_once(EXTENSIONS . '/profiledevkit/content/content.profile.php');
-				
+
 				$context['devkit'] = new Content_ProfileDevkit();
 				self::$active = true;
 			}
 		}
-		
-		public function manipulateDevKitNavigation($context) {
+
+		public function manipulateDevKitNavigation(array $context) {
 			$xml = $context['xml'];
 			$item = $xml->createElement('item');
 			$item->setAttribute('name', __('Profile'));
 			$item->setAttribute('handle', 'profile');
 			$item->setAttribute('active', (self::$active ? 'yes' : 'no'));
-			
+
 			$xml->documentElement->appendChild($item);
 		}
+
+		public function logQuery(array $context) {
+			if(isset($_GET['profile']) && $_GET['profile'] == 'database-queries') {
+				self::$log[$context['query_hash']] = array(
+					'query' => $context['query'],
+					'time' => $context['execution_time']
+				);
+			}
+		}
+
 	}
-	
-?>
--- readme.md
+++ readme.md
@@ -3,9 +3,9 @@
 A profile panel for Symphony.
 It is part of the Symphony core download package.
 
-- Version: 1.0.4
-- Date: 2010-03-19
-- Requirements: Symphony 2.0.4 or later
+- Version: 1.0.5pre
+- Date: unreleased
+- Requirements: Symphony 2.3 or later
 - Author: Rowan Lewis, me@rowanlewis.com
 - GitHub Repository: <http://github.com/rowan-lewis/profiledevkit>
 
